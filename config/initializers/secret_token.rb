d19fe1af63a6b6a130c21cf7fb12db97e4b6208a
Utsav Sarkar <utsav01@SARKAR.local>
2014-03-23 09:22:15 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Aeropals::Application.config.secret_key_base = '7d49e8621175ac8df4551e704e7fe3d9a701967e1dbec2f3d39784f015b72d010534ed0d820d861babb918dc403b9a51af2a64a670f5e8444bec0f058748b4b3'
